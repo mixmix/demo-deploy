499bd01b6f3296340117eacbac439c84af083ed2
mix irving <whimful@gmail.com>
2016-09-08 15:26:47 +1200

    minimal server

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+
+var app = express()
+
+
+app.get('/', function (req, res) {
+
+  res.send('home page')
+})
+
+
+app.get('/cats', function (req, res) {
+
+  res.send('some cats!!!')
+})
+
+app.listen(3000)
+
